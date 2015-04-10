f824fe8a39dc7ae3a6e2e22fa18d9f77b4cc8268
Bertrand Cachet <bertrand.cachet@gmail.com>
2015-04-10 10:38:43 +0200

    FIX: Find JsonCpp header/library in default location

--- cmake/FindJSONCPP.cmake
+++ cmake/FindJSONCPP.cmake
@@ -1,47 +1,49 @@
-# -*- cmake -*-
-# - Find JSONCpp
-# Find the JSONCpp includes and library
+
+# Find jsoncpp
+#
+# Find the jsoncpp includes and library
+# 
+# if you nee to add a custom library search path, do it via via CMAKE_PREFIX_PATH 
+# 
 # This module defines
-#  JSONCPP_INCLUDE_DIRS, where to find json.h, etc.
+#  JSONCPP_INCLUDE_DIRS, where to find header, etc.
 #  JSONCPP_LIBRARIES, the libraries needed to use jsoncpp.
 #  JSONCPP_FOUND, If false, do not try to use jsoncpp.
-#  also defined, but not for general use are
-#  JSONCPP_LIBRARIES, where to find the jsoncpp library.
+#  JSONCPP_INCLUDE_PREFIX, include prefix for jsoncpp
 
-FIND_PATH(JSONCPP_INCLUDE_DIRS json.h
-/usr/include
-/usr/local/include
-${CMAKE_SOURCE_DIR}/win32-deps/include
-PATH_SUFFIXES jsoncpp/json jsoncpp
+# only look in default directories
+find_path(
+	JSONCPP_INCLUDE_DIR 
+	NAMES jsoncpp/json/json.h
+	DOC "jsoncpp include dir"
 )
 
+find_library(
+	JSONCPP_LIBRARY
+	NAMES jsoncpp
+	DOC "jsoncpp library"
+)
 
-FIND_LIBRARY(JSONCPP_LIBRARIES NAMES jsoncpp HINTS /usr/lib /usr/local/lib 	${CMAKE_SOURCE_DIR}/win32-deps/lib)
+set(JSONCPP_INCLUDE_DIRS ${JSONCPP_INCLUDE_DIR})
+set(JSONCPP_LIBRARIES ${JSONCPP_LIBRARY})
 
-IF (JSONCPP_LIBRARIES AND JSONCPP_INCLUDE_DIRS)
-    SET(JSONCPP_LIBRARIES ${JSONCPP_LIBRARIES})
-    SET(JSONCPP_FOUND "YES")
-ELSE (JSONCPP_LIBRARIES AND JSONCPP_INCLUDE_DIRS)
-  SET(JSONCPP_FOUND "NO")
-ENDIF (JSONCPP_LIBRARIES AND JSONCPP_INCLUDE_DIRS)
+# find JSONCPP_INCLUDE_PREFIX
+find_path(
+	JSONCPP_INCLUDE_PREFIX
+	NAMES json.h
+	PATH_SUFFIXES jsoncpp/json json
+)
 
+if (${JSONCPP_INCLUDE_PREFIX} MATCHES "jsoncpp")
+	set(JSONCPP_INCLUDE_PREFIX "jsoncpp/json")
+else()
+	set(JSONCPP_INCLUDE_PREFIX "json")
+endif()
 
-IF (JSONCPP_FOUND)
-   IF (NOT JSONCPP_FIND_QUIETLY)
-      MESSAGE(STATUS "Found JSONCpp: ${JSONCPP_LIBRARIES}")
-   ENDIF (NOT JSONCPP_FIND_QUIETLY)
-ELSE (JSONCPP_FOUND)
-   IF (JSONCPP_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find JSONCPP library include: ${JSONCPP_INCLUDE_DIRS}, lib: ${JSONCPP_LIBRARIES}")
-   ENDIF (JSONCPP_FIND_REQUIRED)
-ENDIF (JSONCPP_FOUND)
-
-# Deprecated declarations.
-SET (NATIVE_JSONCPP_INCLUDE_PATH ${JSONCPP_INCLUDE_DIRS} )
-GET_FILENAME_COMPONENT (NATIVE_JSONCPP_LIB_PATH ${JSONCPP_LIBRARIES} PATH)
-
-MARK_AS_ADVANCED(
-  JSONCPP_LIBRARIES
-  JSONCPP_INCLUDE_DIRS
-  )
+# handle the QUIETLY and REQUIRED arguments and set JSONCPP_FOUND to TRUE
+# if all listed variables are TRUE, hide their existence from configuration view
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(jsoncpp DEFAULT_MSG
+	JSONCPP_INCLUDE_DIR JSONCPP_LIBRARY)
+mark_as_advanced (JSONCPP_INCLUDE_DIR JSONCPP_LIBRARY)
 
